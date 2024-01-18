26215bcdc544933a30f80147f2f314f4f7570f78
hyuntori2000 <131842228+hyuntori2000@users.noreply.github.com>
2024-01-17 18:13:07 -0800

    Add files via upload

---
+++ form_button.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+class FormButton extends StatelessWidget {
+  const FormButton(
+      {super.key,
+      required this.disabled,
+      required this.active,
+      required this.inActive});
+  final String inActive;
+  final String active;
+  final bool disabled;
+
+  @override
+  Widget build(BuildContext context) {
+    return FractionallySizedBox(
+      widthFactor: 1,
+      child: AnimatedContainer(
+        padding: const EdgeInsets.symmetric(
+          vertical: Sizes.size16,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(Sizes.size5),
+          color:
+              disabled ? Colors.grey.shade300 : Theme.of(context).primaryColor,
+        ),
+        duration: const Duration(milliseconds: 300),
+        child: AnimatedDefaultTextStyle(
+          duration: const Duration(milliseconds: 300),
+          style: TextStyle(
+            color: disabled ? Colors.grey.shade400 : Colors.white,
+            fontWeight: FontWeight.w600,
+          ),
+          child: Text(
+            disabled ? inActive : active,
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+    );
+  }
+}
